6c1c43281c7d5b3354689b56a8704640ea882af3
Arron M <marshall.arron@gmail.com>
2019-04-09 10:16:09 -0700

    Add ten event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+document.addEventListener('mouseover', (event) => {
+  console.log('mouseover');
+});
+
+document.addEventListener('keydown', (event) => {
+  console.log('keydown');
+});
+
+document.addEventListener('wheel', (event) => {
+  console.log('wheel');
+});
+
+document.addEventListener('drag', (event) => {
+  console.log('drag');
+});
+
+document.addEventListener('load', (event) => {
+  console.log('load');
+});
+
+document.addEventListener('focus', (event) => {
+  console.log('focus');
+});
+
+document.addEventListener('resize', (event) => {
+  console.log('resize');
+});
+
+document.addEventListener('scroll', (event) => {
+  console.log('scroll');
+});
+
+document.addEventListener('select', (event) => {
+  console.log('select');
+});
+
+document.addEventListener('dblclick', (event) => {
+  console.log('dblclick');
+});
